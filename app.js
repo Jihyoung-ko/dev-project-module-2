50c1eea48adf0fd29ecef719f991bf238b314a3e
koji <koji4386@gmail.com>
2021-05-10 00:00:44 +0200

    added login post route and express session

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 
 mongoose
@@ -19,10 +21,28 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
+const app = express();
+
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: 'mongodb://localhost:27017/dev-project--module2',
+      ttl: 24 * 60 * 60
+    }),
+    secret: process.env.SESS_SECRET,
+    resave: true, 
+    saveUninitialized: true, 
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      }
+  })
+)
+
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-const app = express();
+
 
 // Middleware Setup
 app.use(logger('dev'));
