dd97d50d49bdb6134fe3b4800bd2aab6aa8e3515
Nicolás <nmirabets@me.com>
2021-05-11 21:28:21 +0200

    added company list to homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router  = express.Router();
+const Company = require('../models/company');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  console.log(Company);
+  Company.find({})
+  .then(companies => {
+    res.render('index', { companies });
+  })
+  .catch(error => {
+    next(error);
+  });
 });
 
 module.exports = router;
