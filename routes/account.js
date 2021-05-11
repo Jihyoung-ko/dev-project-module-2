da4625e8aca09a1bcd6a3df67659d8133963b74a
koji <koji4386@gmail.com>
2021-05-11 21:31:08 +0200

    added middleware to the account access

--- routes/account.js
+++ routes/account.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router  = express.Router();
 const User = require('../models/user');
+const userLoggedIn = require('../middleware/middle');
+
 
 
 /* GET account page */
-router.get('/', (req, res, next) => {
+router.get('/', userLoggedIn, (req, res, next) => {
   res.render('account');
 });
 
