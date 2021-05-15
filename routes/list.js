cf482594f3450211902c05b42eef03db3bb4aea6
Nicolás <nmirabets@me.com>
2021-05-15 09:57:42 +0200

    remove company done

--- routes/list.js
+++ routes/list.js
@@ -3,17 +3,34 @@ const router  = express.Router();
 const userLoggedIn = require('../middleware/middle');
 const List = require('../models/list');
 
-/* GET account page */
 router.get('/', userLoggedIn('list'), (req, res, next) => {
   const user = req.session.currentUser
-  List.findById(user._id)
+  List.find({ user: user._id })
+  .populate('company user')
   .then(list => {
-    console.log(list)
+    console.log('list:',list,'user:',user._id)
     res.render('companies/list', { list });
   })
   .catch(error => {
     next(error);
   });
+});
+
+router.post('/:id/add', (req, res, next) => {
+  const user = req.session.currentUser
+  const company = req.params;
+  console.log(company);
+  List.create({
+    user: user._id,
+    company: company.id
+  })
+    .then(list => {
+      // 💥 la url del redirect es completa no como los paths del middleware
+      res.redirect('/');
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
