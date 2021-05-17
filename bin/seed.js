770e7c4900991fad556e46ef8af6ad7da63a0737
Nicolás <nmirabets@me.com>
2021-05-17 18:59:07 +0200

    update seed file

--- bin/seed.js
+++ bin/seed.js
@@ -1,8 +1,26 @@
 const mongoose = require('mongoose');
+//onst axios = require('axios');
+//const csvToJson = require('convert-csv-to-json');
+//onst fileDownload = require('js-file-download');
 const Company = require('../models/company');
-const tickerList = require('./ticker-list.json');
+const tickerList = require('./ticker-list.js');
 
-console.log(tickerList);
+// const arkFundNames = [
+//   'ARK_INNOVATION_ETF_ARKK_HOLDINGS',]
+//   'ARK_AUTONOMOUS_TECHNOLOGY_&_ROBOTICS_ETF_ARKQ_HOLDINGS',
+//   'ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS',
+//   'ARK_GENOMIC_REVOLUTION_MULTISECTOR_ETF_ARKG_HOLDINGS',
+//   'ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS',
+//   'ARK_SPACE_EXPLORATION_&_INNOVATION_ETF_ARKX_HOLDINGS'
+// ];
+
+// create company json list
+const companyList = [];
+tickerList.forEach(element => {
+  companyList.push({
+    ticker: element,
+  })
+})
 
 mongoose
   .connect('mongodb://localhost:27017/dev-project--module2', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -11,7 +29,7 @@ mongoose
   })
   .then(() => 
     Company.create(
-      tickerList
+      companyList
     )
   )
   .then(() => {
@@ -23,3 +41,21 @@ mongoose
   .finally(() => {
     mongoose.connection.close();
   });
+
+  // get ark fund data ***WIP***
+  
+  // .then(() => {
+  //   // get Ark holdings
+  //   const promiseArray = [];
+  //   arkFundNames.forEach( (fundName) => {
+  //     promiseArray.push(axios.get('https://ark-funds.com/wp-content/fundsiteliterature/csv/' + fundName + '.csv', { responseType: 'blob',}))
+  //   })
+  //   // execute promises array
+  //   return Promise.all(arrayPromises)
+  // })
+  // .then((promisesRes, index) => {
+
+  //   //console.log('data:', promisesRes[0].data)
+
+  //   fileDownload(promisesRes[0].data, 'report.csv')
+  // })
