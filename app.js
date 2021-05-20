2c94fd7f94cfa327a7f174b62a27c9c43860a2a3
koji <koji4386@gmail.com>
2021-05-20 19:14:01 +0200

    comment out node-sass

--- app.js
+++ app.js
@@ -50,11 +50,11 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
       
 
 app.set('views', path.join(__dirname, 'views'));
